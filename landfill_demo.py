d855410abf02ee7bde4b5e75c9c0ebead1939c0a
Pedro Anchieta <phanchieta@gmail.com>
2019-04-15 18:14:55 -0400

    minor changes

--- landfill_demo.py
+++ landfill_demo.py
@@ -2,30 +2,37 @@ from process_model import *
 from full_system import *
 from process_model_output import *
 import material_properties
+import time
+import datetime
 
 
 
 
 
-
-del databases["Waste"]
+#del databases["Waste"]
 a = ProcessModelOutput()
 a.create_waste_technosphere()
 print('Importing SWOLF data')
 SWOLF_data = a.read_output_from_SWOLF ("Landfill", "trad_landfill.xlsx")
-tr = Treatment('Landfill',['a','b','c'],['a','b','c'],{},{},['a','b','c'],'material properties here') 
+tr = Treatment('Landfill',['a','b','c'],['a','b','c'],{},{},['a','b','c'],'more material properties here')
 tr.import_from_SWOLF(SWOLF_data)
+data = tr.get_output()
+tr.write_output('landfill.csv')
 
-tr.write_output('test.csv')
 print('Importing Full System')
-fs = FullSystem('test.csv','database_TT2.csv')
+fs = FullSystem('landfill.csv','database_TT3.csv')
 print('Running Full System')
-fs.run_no_TT()
-print('Running LCA')
-
-db = Database("Waste")
-functional_unit = {db.get("Initial") : 1}
-lca = LCA(functional_unit, ('IPCC 2013', 'climate change', 'GWP 100a')) 
-lca.lci()
-lca.lcia()
-lca.score
+t1 = time.time()
+fs.run_no_TT_from_memory(data)
+t2 = time.time()
+print('Time to run full system: %0.1f secs' % (t2-t1))
+#
+#
+#
+#
+#db = Database("Waste")
+#functional_unit = {db.get("Initial") : 1}
+#lca = LCA(functional_unit, ('IPCC 2013', 'climate change', 'GWP 100a')) 
+#lca.lci()
+#lca.lcia()
+#lca.score
