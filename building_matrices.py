2e7d713d59e2077e5735a25e0076be97ebcf9d1e
msm_sardar <msardar2@ncsu.edu>
2020-02-26 16:46:37 -0500

    Optimization -> new constraint type <= and >=

--- building_matrices.py
+++ building_matrices.py
@@ -25,7 +25,6 @@ else:
     pool_adapter = lambda x: x
 
 
-
 def worker(args):
     project, functional_unit, method, parameters, process_models, process_model_names, common_data, tech_matrix, bio_matrix, seed , n = args
     projects.set_current(project, writable=False)
@@ -117,6 +116,7 @@ def parallel_mc (lca, project, functional_unit, method, tech_matrix, bio_matrix,
     
   
 
+#%% Monte Carlo simulation
 
 class ParallelData(LCA):
     def __init__(self, functional_unit, method, project, process_models = None, process_model_names = None, common_data = None, parameters = None,seed = None):
@@ -133,8 +133,8 @@ class ParallelData(LCA):
         self.seed = seed
         
         
+        self.activities_dict,_,self.biosphere_dict = self.reverse_dict()
         
-        activities_dict = dict(zip(self.activity_dict.values(),self.activity_dict.keys()))
         """
         tech_matrix is dictionary include all the exhange as tuple (product,Feed) key and amount as value
         {(('LF', 'Aerobic_Residual'), ('SF1_product', 'Aerobic_Residual_MRDO')):0.8288506683507344}
@@ -143,10 +143,9 @@ class ParallelData(LCA):
         """
         self.tech_matrix = dict()
         for i in self.tech_params:
-            self.tech_matrix[(activities_dict[i[2]], activities_dict[i[3]])] = i[6]
+            self.tech_matrix[(self.activities_dict[i[2]], self.activities_dict[i[3]])] = i[6]
         
         
-        biosphere_dict = dict(zip(self.biosphere_dict.values(),self.biosphere_dict.keys()))
         self.bio_matrix = dict()
         """
         bio_matrix is dictionary include all the exhange as tuple (product,Feed) key and amount as value
@@ -156,10 +155,10 @@ class ParallelData(LCA):
         """
         
         for i in self.bio_params:
-            if (biosphere_dict[i[2]], activities_dict[i[3]]) not in self.bio_matrix.keys():
-                self.bio_matrix[(biosphere_dict[i[2]], activities_dict[i[3]])] = i[6]
+            if (self.biosphere_dict[i[2]], self.activities_dict[i[3]]) not in self.bio_matrix.keys():
+                self.bio_matrix[(self.biosphere_dict[i[2]], self.activities_dict[i[3]])] = i[6]
             else:
-                self.bio_matrix[(str(biosphere_dict[i[2]]) + " - 1", activities_dict[i[3]])] = i[6]
+                self.bio_matrix[(str(self.biosphere_dict[i[2]]) + " - 1", self.activities_dict[i[3]])] = i[6]
                 #print((str(biosphere_dict[i[2]]) + " - 1", activities_dict[i[3]]))                
         
     def run(self, nproc, n):       
@@ -174,155 +173,193 @@ class ParallelData(LCA):
         self.results = [x for lst in res for x in lst]
         #res=worker((self.project, self.functional_unit, self.method, self.parameters, self.process_models, self.process_model_names, self.common_data, self.tech_matrix, self.bio_matrix, self.seed, n//nproc))
         #self.results = [x for lst in res for x in lst]
-        
-    def objective_function(self, x):
-        matrix=self.project.unified_params.get_matrix(x)
-        for key, value in matrix.items():
-            if key in self.tech_matrix:
-                self.tech_matrix[key] = value    
-    
-        tech = np.array(list(self.tech_matrix.values()), dtype=float)
-        bio = np.array(list(self.bio_matrix.values()), dtype=float)
-        
-        self.rebuild_technosphere_matrix(tech)
-        self.rebuild_biosphere_matrix(bio)
-        self.lci_calculation()
-        if self.lcia:
-            self.lcia_calculation()
 
-        return self.score/10**self.magnitude
-    
-    
-    def create_equality(self, count):
-        local_count = self.running_count
-        l = lambda x: sum([x[i] for i in range(local_count,local_count+count)]) -1 
-        self.running_count+=count
-        return l
+    ### Export results
+    def result_to_DF(self):
+        output=pd.DataFrame()
+        # Reporting the LCIA results; Create a column for each method
+        for j in self.results[0][1].keys():
+            output[j] = [self.results[i][1][j] for i in range(len(self.results))]
+        # Reporting the input data    
+        for j in range(len(self.results[0][2])):
+                output[self.results[0][2][j][0]] = [self.results[i][2][j][1] for i in range(len(self.results))]
+        return(output)
+
+    def save_results(self,name):
+        self.result_to_DF().to_pickle(name)
+
+
+
+#%% Optimization
+# =============================================================================
+# =============================================================================
+    ### Optimization of mass fractions
+# =============================================================================
+# =============================================================================
+    ### Objective function    
+    def objective_function(self, x):
+        """
+        Use the new parameters (Waste fractions) to update the tech_matrix (tech_param)
+        and reculate the LCA score
+        """
+        if self.oldx != list(x): # Calculations are done only when the function get new x.
+            matrix=self.project.unified_params.get_matrix(x)
+            for key, value in matrix.items():
+                if key in self.tech_matrix:
+                    self.tech_matrix[key] = value    
         
-    def create_inequality(self, key, val, mass_flow=None):
-        if mass_flow:
-            l = lambda x: val - self.get_mass_flow(key, x)
-            return l 
-        else:
-            l = lambda x: val - self.get_emission_amount(key, x)
-            return l
-    
-    def get_mass_flow(self, process_name, x):
+            tech = np.array(list(self.tech_matrix.values()), dtype=float)
+            bio = np.array(list(self.bio_matrix.values()), dtype=float)
+            
+            self.rebuild_technosphere_matrix(tech)
+            self.rebuild_biosphere_matrix(bio)
+            self.lci_calculation()
+            if self.lcia:
+                self.lcia_calculation()
+            
+            self.oldx = list(x)
+        return(self.score/10**self.magnitude)
+
+    ### Mass to process
+    def get_mass_flow(self, key, KeyType, x):
+        """
+        calculate the mass to the process from the supply_array matrix
+        """
         self.objective_function(x)
         
-        system=self.solve_linear_system()
-        flow_dict = dict()
         mass_flow=0
-        j=0
+        if KeyType == 'WasteToProcess':
+            for i in range(len(self.supply_array)):
+                if key == self.activities_dict[i]:
+                    mass_flow += self.supply_array[i]
         
-        for i in self.activity_dict.keys():
-            if system[j]!=0:
-                flow_dict[i]=system[j]
-            j+=1
-        
-        if type(process_name) == tuple:
-            for i in flow_dict.keys():
-                if process_name == i:
-                    mass_flow += flow_dict[i]
+        elif KeyType == 'Process':
+            for i in range(len(self.supply_array)):
+                if key == self.activities_dict[i][0]:
+                    mass_flow += self.supply_array[i]
         else:
-            for i in flow_dict.keys():
-                if process_name == i[0]:
-                    mass_flow += flow_dict[i]
-                
+            raise ValueError(""" KeyType for the get_mass_flow function is not defined correct.""")
         return mass_flow
         
+    ### Emission flow in LCI
     def get_emission_amount(self, emission, x):
         self.objective_function(x)
-        
-        lci_bio=self.biosphere_matrix*self.supply_array
-        emission_dict = dict()
+        inventory=self.biosphere_matrix*self.supply_array
         emission_amount = 0
-        j =0
-        
-        for i in self.biosphere_dict.keys():
-            if lci_bio[j]!=0:
-                emission_dict[i]=lci_bio[j]
-            j+=1
-            
-        for i in emission_dict.keys():
-            if emission == i[1]:
-                emission_amount+=emission_dict[i]
-        
+        for i in range(len(inventory)):
+            if emission == self.biosphere_dict[i]:
+                emission_amount+=inventory[i]
         return emission_amount
+    
+    
+    def create_equality(self, N_param_Ingroup):
+        """
+        Check that the sum of parameters in each group should be one.
+        """
+        local_index = self.Param_index
+        l = lambda x: sum([x[i] for i in range(local_index,local_index+N_param_Ingroup)]) -1 
+        self.Param_index+=N_param_Ingroup
+        return l
         
-        
+    def create_inequality(self, key, limit, KeyType,ConstType):
+        """
+        key: process name, key for activtity in process or key for activity in biosphere
+        ConstType: <= , >=
+        KeyType: Process,WasteToProcess,Emission
+        """
+        if ConstType not in ['<=','>=']:
+            raise ValueError(" Constraint Type is not defined correct ") 
+                        
+        if KeyType == 'Process':
+            if ConstType == '<=':
+                l = lambda x: limit - self.get_mass_flow(key, KeyType, x) 
+            else:
+                l = lambda x: self.get_mass_flow(key, KeyType, x) - limit
+            return l
+            
+        elif KeyType == 'WasteToProcess':
+            if ConstType == '<=':
+                l = lambda x: limit - self.get_mass_flow(key, KeyType, x)
+            else:
+                l = lambda x: self.get_mass_flow(key, KeyType, x) - limit
+            return l
+
+        elif KeyType == 'Emission': 
+            if ConstType == '<=':
+                l = lambda x: limit - self.get_emission_amount(key, x)
+            else:
+                l = lambda x: self.get_emission_amount(key, x) - limit
+            return l
+            
     
     def create_constraints(self):
         cons = list()
         group = dict()
-        self.running_count=0
         
+        # Index for the parameters
+        self.Param_index=0
+        
+        # Number of parameters in each group (from one source to different denstinations)
         for key in self.project.unified_params.param_uncertainty_dict.keys():
             group[key] = len(self.project.unified_params.param_uncertainty_dict[key])
 
+        # Equal constraint (sum of the parameters in each group should be one)
         for vals in group.values():
-            cons.append({'type':'eq', 'fun':self.create_equality(vals)})
+            cons.append({'type':'eq', 'fun':self.create_equality(N_param_Ingroup=vals)})
             
-        if self.mass_flows_constraints:
-            for key in self.mass_flows_constraints.keys():
-                cons.append({'type':'ineq', 'fun': self.create_inequality(key, self.mass_flows_constraints[key], mass_flow=True)})
-        
-        if self.emissions_constraints:
-            for key in self.emissions_constraints.keys():
-                cons.append({'type':'ineq', 'fun': self.create_inequality(key, self.emissions_constraints[key])})
-                
-        
+        if self.constraints:
+            for key in self.constraints.keys():
+                cons.append({'type':'ineq', 'fun': self.create_inequality(key, self.constraints[key]['limit'], self.constraints[key]['KeyType'], self.constraints[key]['ConstType'])})
         return cons
-        
-    def optimize_parameters(self, project, mass_flows_constraints=None, emissions_constraints=None):
     
-        self.mass_flows_constraints=mass_flows_constraints
-        self.emissions_constraints=emissions_constraints
+    def optimize_parameters(self, project, constraints=None):
+        self.constraints=constraints
         self.project = project
         self.magnitude = len(str(int(abs(self.score)))) 
         
-        x0 = [i['amount'] for i in self.project.parameters_list] #initializing with the users initial solution
+        x0 = [i['amount'] for i in self.project.parameters_list] # initializing with the users initial solution
         #x0 = [1 for _ in self.project.parameters_list] #changing initial x0 to outside feasible region
-        bnds = tuple([(0,1) for _ in self.project.parameters_list])
-        cons = self.create_constraints()
         
-        res = minimize(self.objective_function, x0, method='SLSQP', bounds=bnds, constraints=cons)
+        self.oldx=[0 for i in range(len(x0))]
+        
+        bnds = tuple([(0,1) for _ in self.project.parameters_list])
+        self.cons = self.create_constraints()
+        
+        res = minimize(self.objective_function, x0, method='SLSQP', bounds=bnds, constraints=self.cons)
         if res.success:
             self.success=True
             self.optimized_x=list()
             res.x=res.x.round(decimals=3)
             for i in range(len(self.project.parameters_list)):
                 self.optimized_x.append({'name':self.project.parameters_list[i]['name'],'amount':res.x[i]})
+            print(self.optimized_x)
             return res
         else:
             self.success=False
             print(res.message)
             return res
             
-    def multi_start_optimization(self, project, mass_flows_constraints=None, emissions_constraints=None, max_iter=30):
-        
-        self.mass_flows_constraints=mass_flows_constraints
-        self.emissions_constraints=emissions_constraints
+    def multi_start_optimization(self, project, constraints=None, max_iter=30):
+        self.constraints=constraints
         self.project = project
         self.magnitude = len(str(int(abs(self.score)))) 
         
         global_min = 1E100
         
+        self.cons = self.create_constraints()
+        bnds = tuple([(0,1) for _ in self.project.parameters_list])
         
         for _ in range(max_iter):
-        
             x0 = [random() for i in self.project.parameters_list] #initializing with the users initial solution
-            bnds = tuple([(0,1) for _ in self.project.parameters_list])
-            cons = self.create_constraints()
-        
-            res = minimize(self.objective_function, x0, method='SLSQP', bounds=bnds, constraints=cons)
+            self.oldx=[0 for i in range(len(x0))]
+            res = minimize(self.objective_function, x0, method='SLSQP', bounds=bnds, constraints=self.cons)
             
             if res.success:
                 if res.fun < global_min:
                     res_global = res
                     global_min = res.fun
+                    print(global_min)
         
-            
         if res_global.success:
             self.success=True
             self.optimized_x=list()
@@ -335,28 +372,21 @@ class ParallelData(LCA):
             print(res_global.message)
             return res_global
     
-    
-    
     def set_optimized_parameters_to_project(self):
         assert hasattr(self, "project"), "Must run optimize_parameters first"
         assert self.success, "Optimization has to be sucessful first"
         
         self.project.update_parameters(self.optimized_x)
+    
+# =============================================================================
+#     def mass_to_process(self):
+#         get_mass_flow(self, key, KeyType, x):
+# =============================================================================
+        
         
     
     
-    def result_to_DF(self):
-        output=pd.DataFrame()
-### Reporting the LCIA results; Create a column for each method
-        for j in self.results[0][1].keys():
-            output[j] = [self.results[i][1][j] for i in range(len(self.results))]
-### Reporting the input data    
-        for j in range(len(self.results[0][2])):
-                output[self.results[0][2][j][0]] = [self.results[i][2][j][1] for i in range(len(self.results))]
-        return(output)
 
-    def save_results(self,name):
-        self.result_to_DF().to_pickle(name)
     
   
 
