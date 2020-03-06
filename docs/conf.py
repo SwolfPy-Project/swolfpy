9068afceb889ea3148c7deeb2dee565099ba3124
Mojtaba Sardarmehni <msardar2@ncsu.edu>
2020-03-06 01:36:13 +0000

    conf.py edited online with Bitbucket

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
+
+autodoc_mock_imports["pandas"]
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
