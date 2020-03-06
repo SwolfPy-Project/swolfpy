666133ad7a5b5c45c3005efce4ca3c5e1ec05ee5
Mojtaba Sardarmehni <msardar2@ncsu.edu>
2020-03-06 06:50:37 +0000

    conf.py edited online with Bitbucket

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 
 
-autodoc_mock_imports = ["PySWOLF",'pandas','pd','Distance','PySWOLF.Distance','Brightway2','LCA','PySWOLF.UI.PySWOLF_run', 'PySide2','PySide2.QtGui', 'PySide2.QtCore', 'PySide2.QtWidgets']
+#autodoc_mock_imports = ["PySWOLF",'pandas','pd','Distance','PySWOLF.Distance','Brightway2','LCA','PySWOLF.UI.PySWOLF_run', 'PySide2','PySide2.QtGui', 'PySide2.QtCore', 'PySide2.QtWidgets']
 
 import os
 import sys
