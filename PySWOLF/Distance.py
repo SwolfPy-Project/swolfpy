839e41e66f9bab4c66a2602356bd5f7d961c1ca2
msm_sardar <msardar2@ncsu.edu>
2020-03-07 20:43:06 -0500

    Document for Distance class

--- PySWOLF/Distance.py
+++ PySWOLF/Distance.py
@@ -12,15 +12,28 @@ class Distance():
 
     """
     def __init__(self,path=None,Data=None):
-        """Create Distance class.
+        """Create Distance class.            
 
-        Args:
-            * *path* (str,optional): Path to the *csv* file.
-            * *Data* (pd.DataFrame, optional): Padans Dataframe for the distances between the process models. Dataframe should use the name of processes as both column and row index.
+        :param path: Path to the *csv* file
+        :type path: str, optional
+        :param Data: Padans Dataframe for the distances between the process models.Dataframe should use the name of processes as both column and row index.
+        :type Data: class: `pandas.DataFrame` , optional
 
-        Returns:
-            A new Distance object.
-
+        :Example:
+        
+        >>> from PySWOLF.Distance import Distance
+        >>> import pandas as pd
+        >>> Processes = ['LF','WTE','AD']
+        >>> Data = pd.DataFrame([[None,20,30],[None,None,10],[None,None,None]],index=Processes,columns=Processes)
+        >>> Data
+               LF   WTE    AD
+        LF   None  20.0  30.0
+        WTE  None   NaN  10.0
+        AD   None   NaN   NaN
+        >>> distance = Distance(Data=Data)
+        >>> distance.Distance[('LF','WTE')]
+        20.0
+        
         """
         if path:
             self.data = pd.read_csv(path,index_col='Index')
@@ -35,13 +48,3 @@ class Distance():
                         self.Distance[(j,i)] = self.data[i][j]
                         if not pd.isna(self.data[j][i]) and self.data[j][i]!='' and self.data[j][i]!=self.data[i][j]:
                             raise Exception(f'Distance from {i} to {j} is not equal to distance from {j} to {i}')
-                            
-        """
-        tets
-        """
-    """
-    test indent
-    """
-"""
-test double intent
-"""
