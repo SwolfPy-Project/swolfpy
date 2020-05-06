fe39569078d25687d47908bc19cd7adc973a175b
Mojtaba Sardar <msardar2@ncsu.edu>
2020-05-06 01:05:03 -0400

    test pytest

--- tests/test_swolfpy.py
+++ tests/test_swolfpy.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
 """Tests for `swolfpy` package."""
-
-
+import pytest
 from swolfpy import *
 from swolfpy.ProcessModels import LF
 
 
-class TestPyswolf():
-    def test():
+class test_swolfpy():
+    def test_LF():
         A=LF.LF()
         A.calc()
         B=A.report()
-        assert len(B)==3
+        assert len(B)==4
       
     
 
