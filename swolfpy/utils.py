6a263ff9a58c6e282e3137bf1340551e35fb8a8d
Mojtaba Sardarmehni <msardar2@ncsu.edu>
2021-08-13 17:20:56 -0400

    add find_biosphere_flows func

--- swolfpy/utils.py
+++ swolfpy/utils.py
@@ -43,6 +43,8 @@ def dump_method(methodName, path=None):
     for i in data:
         key.append(i[0])
         value.append(i[1])
+        if 'unit' not in method.metadata:
+            method.metadata['unit'] = None
         unit.append(method.metadata['unit'])
         act = bw2.get_activity(i[0])
         name.append(act.as_dict()['name'])
@@ -57,3 +59,34 @@ def dump_method(methodName, path=None):
     else:
         DF.to_csv(str(methodName) + '.csv', index=False)
     return(DF)
+
+
+def find_biosphere_flows(flow_name, compartment=None, subcompartment=None, exact_match=True):
+    """
+    Finds flows in the biosphere and reports the keys from biosph.
+    """
+    key = []
+    name = []
+    categories = []
+    db = bw2.Database('biosphere3')
+    for act in db:
+        act_dict = act.as_dict()
+        if (exact_match and act_dict['name'] == flow_name) or (not exact_match and flow_name.lower() in act_dict['name'].lower()):
+            if compartment and subcompartment:
+                if act_dict['categories'] != (compartment, subcompartment):
+                    key.append(act.key)
+                    name.append(act_dict['name'])
+                    categories.append(act_dict['categories'])
+            elif compartment:
+                if act_dict['categories'][0] == compartment:
+                    key.append(act.key)
+                    name.append(act_dict['name'])
+                    categories.append(act_dict['categories'])
+            else:
+                key.append(act.key)
+                name.append(act_dict['name'])
+                categories.append(act_dict['categories'])
+    DF = pd.DataFrame({'key': key,
+                       'name': name,
+                       'categories': categories})
+    return DF
